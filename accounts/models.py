6547f394468d704ac82ce20016c9d573b5846bb4
Emmanuel BIZIMANA <emmabnatha7@gmail.com>
2025-03-04 07:00:12 +0200

    Initial project setup with Django and custom User model

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)#hash it later
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
