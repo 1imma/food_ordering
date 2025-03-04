719a1aeb0e8832116cdfff120637ca1f6c0b3dbd
Emmanuel BIZIMANA <emmabnatha7@gmail.com>
2025-03-04 07:59:57 +0200

    Signup Implentation(views, urls, and connect to html)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User
 
-# Create your views here.
+# Signup view
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists:
+            messages.error('Username taken')
+            return redirect('signup')
+        
+        if User.objects.filter(email=email).exists:
+            messages.error('Email is registered')
+            return redirect('signup')
+
+        user = User(username=username, email=email, password=password)
+        user.save()
+
+        messages.success('Created Account Successfully, please login')
+        return redirect('login')
+    return render(request, 'accounts/signup.html')
+
+
